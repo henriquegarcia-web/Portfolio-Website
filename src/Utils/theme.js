e8374ffdf7c8de65b958a5459ba2281e3dbbe68d
Henrique Garcia <https.henriquegarcia@gmail.com>
2022-05-17 05:42:39 -0300

    Definição Inicial das Cores do Tema

--- src/Utils/theme.js
+++ src/Utils/theme.js
@@ -1,9 +1,15 @@
 export const lightTheme = {
-  body: '#fff',
-  fontColor: '#000',
+  body: 'rgba(240, 240, 240, 1)',
+  elements_light: 'rgba(250, 250, 250, 1)',
+  elements_dark: 'rgba(230, 230, 230, 1)',
+  variant: 'rgba(247, 145, 49, 1)',
+  fontColor: 'rgba(10, 10, 10, 1)',
 }
 
 export const darkTheme = {
-  body: '#000',
-  fontColor: '#fff',
+  body: 'rgba(36, 40, 46, 1)',
+  elements_light: 'rgba(57, 61, 66, 1)',
+  elements_dark: 'rgba(30, 34, 37, 1)',
+  variant: 'rgba(247, 145, 49, 1)',
+  fontColor: 'rgba(240, 240, 240, 1)',
 }
