21b91afa312015385f8f5fdd5c8d1492a09a4d57
Henrique Garcia <https.henriquegarcia@gmail.com>
2022-05-18 05:14:37 -0300

    Composição da View de MyProjects

--- src/Pages/Home/Views/MyProjects/index.jsx
+++ src/Pages/Home/Views/MyProjects/index.jsx
@@ -2,7 +2,118 @@ import React from 'react'
 import * as G from '../../../../Utils/globals'
 import * as S from './style'
 
+import ImgBmw from '../../../../Assets/MyProjects/BMW.png'
+import ImgDelivery from '../../../../Assets/MyProjects/DELIVERY.png'
+import ImgDashboard from '../../../../Assets/MyProjects/DASHBOARD.png'
+import ImgFelab from '../../../../Assets/MyProjects/FELAB.png'
+import ImgMonky from '../../../../Assets/MyProjects/MONKY.png'
+import ImgPanel from '../../../../Assets/MyProjects/PANEL.png'
+import ImgQuiz from '../../../../Assets/MyProjects/QUIZ.png'
+import ImgS1000RR from '../../../../Assets/MyProjects/S1000RR.png'
+import ImgVegan from '../../../../Assets/MyProjects/VEGAN.png'
+
 const MyProjects = () => {
+
+  const MyProjects = [
+    {
+      image: ImgBmw,
+      title: 'Desafio Front-End React',
+      github: 'https://github.com/henriquegarcia-web/Vnda-Frontend-Challenge',
+      behance: 'https://www.behance.net/gallery/143798089/Front-End-Challenge-com-tema-da-BMW',
+      tags: [
+        'JavaScript',
+        'HTML',
+        'SASS'
+      ]
+    },
+    {
+      image: ImgPanel,
+      title: 'Desafio Front-End React Pleno',
+      github: 'https://github.com/henriquegarcia-web/TaugorApp',
+      behance: 'https://www.behance.net/gallery/142367647/Teste-Front-End-Pleno',
+      tags: [
+        'React',
+        'Styled Components',
+        'Material UI',
+        'Firebase'
+      ]
+    },
+    {
+      image: ImgDashboard,
+      title: 'Dashboard com React & Material UI',
+      github: 'https://github.com/henriquegarcia-web/personal-blog-firebase-v1',
+      behance: 'https://www.behance.net/gallery/142367199/Deashboard-V1-React-Material-UI',
+      tags: [
+        'React',
+        'Styled Components',
+        'Bootstrap'
+      ]
+    },
+    {
+      image: ImgMonky,
+      title: 'Agência Monky Website',
+      github: 'https://github.com/henriquegarcia-web/AgenciaMonky',
+      behance: 'https://www.behance.net/gallery/142366503/Agencia-Monky-Website',
+      tags: [
+        'React',
+        'Styled Components',
+      ]
+    },
+    {
+      image: ImgFelab,
+      title: 'Front-End Lab - Projeto em desenvolvimento',
+      github: 'https://github.com/henriquegarcia-web/Front-End-Lab',
+      behance: 'https://www.behance.net/gallery/143993811/Front-End-Lab',
+      tags: [
+        'React',
+        'Styled Components',
+        'Firebase',
+      ]
+    },
+    {
+      image: ImgS1000RR,
+      title: 'BMW S 1000 RR - Website de Apresentação da Moto',
+      github: 'https://github.com/henriquegarcia-web/S1000RR-Presentation-Website',
+      behance: 'https://www.behance.net/gallery/143796439/BMW-S-1000-RR-Website-de-Apresentacao-da-Moto',
+      tags: [
+        'JavaScript',
+        'HTML',
+        'LESS'
+      ]
+    },
+    {
+      image: ImgQuiz,
+      title: 'DevQuiz Layout HTML & Less',
+      github: 'https://github.com/henriquegarcia-web/DevQuiz',
+      behance: 'https://www.behance.net/gallery/143795339/DevQuiz-Layout-HTML-Less',
+      tags: [
+        'JavaScript',
+        'HTML',
+        'LESS'
+      ]
+    },
+    {
+      image: ImgDelivery,
+      title: 'Aplicação Mobile de Delivery',
+      github: 'https://github.com/henriquegarcia-web/delivery-app-template',
+      behance: 'https://www.behance.net/gallery/143993147/Delivery-App',
+      tags: [
+        'React',
+        'Styled Components',
+      ]
+    },
+    {
+      image: ImgVegan,
+      title: 'One Page Site | Restaurante Vegano',
+      github: 'https://github.com/henriquegarcia-web/VEGAN-CATALOG-REACT',
+      behance: 'https://www.behance.net/gallery/131155837/One-Page-Site-Restaurante-Vegano',
+      tags: [
+        'React',
+        'Styled Components',
+      ]
+    },
+  ]
+
   return (
     <S.MyProjects>
       <G.ViewHeader>
@@ -12,6 +123,26 @@ const MyProjects = () => {
       </G.ViewHeader>
       <G.ViewContent>
         <S.MyProjectsContent>
+
+          {MyProjects.map(({ image, title, github, behance, tags, index }) => (
+            <S.ProjectContainer key={index}>
+              <S.ProjectImageContainer>
+                <S.ProjectImage src={image} alt={title} />
+              </S.ProjectImageContainer>
+              <S.ProjectDetails className='details'>
+                <S.ProjectTitle>{title}</S.ProjectTitle>
+                <S.ProjectLinks>
+                  <S.Link href={github} target='_blank'>GitHub</S.Link>
+                  <S.Link href={behance} target='_blank'>Behance</S.Link>
+                </S.ProjectLinks>
+                <S.ProjectTags>
+                  {tags.map((tag) => (
+                    <G.Tag key={tag}>{tag}</G.Tag>
+                  ))}
+                </S.ProjectTags>
+              </S.ProjectDetails>
+            </S.ProjectContainer>
+          ))}
 
         </S.MyProjectsContent>
       </G.ViewContent>
--- src/Pages/Home/Views/MyProjects/style.js
+++ src/Pages/Home/Views/MyProjects/style.js
@@ -12,11 +12,84 @@ export const MyProjects = styled.div`
 `
 
 export const MyProjectsContent = styled.div`
-  display: flex;
-  /* flex-direction: column; */
-  /* align-items: center; */
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-row-gap: 20px;
+  grid-column-gap: 20px;
   width: 100%;
   height: 100%;
+  padding: 15px;
+`
 
-  /* border: 1px solid orange; */
+export const ProjectContainer = styled.div`
+  position: relative;
+  
+  &:hover {
+    img {
+      filter: blur(3px);
+      opacity: 0.7;
+    }
+
+    .details {
+      display: flex;
+    }
+  }
+`
+
+export const ProjectImageContainer = styled.div`
+  z-index: 1;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+export const ProjectImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  cursor: pointer;
+  transition: .2s;  
+`
+
+export const ProjectDetails = styled.div`
+  z-index: 2;
+  position: absolute;
+  display: none;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  padding: 15px;
+  /* pointer-events: none; */
+`
+
+export const ProjectTitle = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 22px;
+  margin-bottom: 10px;
+
+  color: ${props => props.theme.font_color};
+`
+
+export const ProjectLinks = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`
+
+export const Link = styled.a`
+  width: 50%;
+  font-size: 13px;
+  font-weight: 500;
+
+  color: ${props => props.theme.font_color};
+`
+
+export const ProjectTags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: auto;
 `
